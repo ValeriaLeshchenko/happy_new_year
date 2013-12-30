25bef04eae0c9f116464a2e2912e5a8d2750cea8
Valeria <atargatis.the.one@gmail.com>
2013-12-30 16:47:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyNewYear::Application.config.secret_key_base = 'd12584fd826724be08a800d80833c83905472f4bd517fffbea630b7d4bb4aaeaefac8ccd94d350795fd795668caa1867c76c2397bcc0750a89ed7cad30423020'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HappyNewYear::Application.config.session_store :cookie_store, key: '_happy_new_year_session'
