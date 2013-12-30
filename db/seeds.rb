c43e845564f096bcc4a725c01b0d18c25ed221a5
Valeria <atargatis.the.one@gmail.com>
2013-12-31 00:19:46 +0200

    Edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-special = Special.create(name: "Dima")
+special = Special.create(name: "Дима")
